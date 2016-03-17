463a684c1b199c49c8d255cc0db11a2815eeef57
zhangzhuobin <zhangzhuobin@baidu.com>
2016-03-17 17:05:40 +0800

    Build

---
+++ src/demo.js
@@ -0,0 +1,21 @@
+import './style.scss'
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import Mover from '../lib/mover.js'
+
+class Demo extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render () {
+        return (
+            <Mover className="mover" style={{display: 'inline-block', position: 'relative'}}>
+                <div>Drag me.</div>
+            </Mover>
+        )
+    }
+}
+
+let root = document.createElement('div')
+document.body.appendChild(root)
+ReactDOM.render(<Demo />, root)
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const autoprefixer = require('autoprefixer')
+const path = require('path')
+
+module.exports = {
+  context: path.join(__dirname, './src'),
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000',
+    'webpack/hot/only-dev-server',
+    './demo.js',
+  ],
+  output: {
+    path: path.join(__dirname, './dist'),
+    publicPath: "/",
+    filename: 'demo.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.js?$/, exclude: /node_modules/, loader: "react-hot!babel"},
+      {test: /\.scss$/, exclude: /node_modules/, loader: "style!css!postcss!sass"}
+    ]
+  },
+  postcss: [autoprefixer],
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, "./node_modules")]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'react-mover',
+      filename: '../index.html',
+    //   template: '../templates/index.html'
+    }),
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': '"development"'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
